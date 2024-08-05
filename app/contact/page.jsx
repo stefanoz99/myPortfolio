9591620bd0505e136ba439129b01a8aa92638744
Manolo Iniguez <iniguezms@hotmailcom>
2024-08-05 14:48:48 -0500

    Remove Globe at Contact Page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -97,23 +97,9 @@ const Contact = () => {
 
             </form>
           </div>
-
-          <div className="xl:w-[46%] order-1 xl:order-1 flex flex-col gap-[30px]">
-
-            {/* globe */}
-            <div className="rounded-xl justify-end">
-              <p className="text-xl text-white/80 mb-4 text-center">
-                I'm very flexible with time zone communications
-              </p>
-              <div className="relative rounded-xl w-full h-64 overflow-hidden justify-center ithems-center">
-                <div className="absolute w-full h-full top-1/2 transform -translate-y-1/2 scale-95">
-                  <GlobeDemo />
-                </div>
-              </div>
-            </div>
-
-            {/* info */}
-            <div className="flex-1 flex items-center xl:justify-center mb-8 xl:mb-0 order-1 xl:order-none">
+          
+          {/* info */}
+          <div className="flex-1 flex items-center xl:justify-center mb-8 xl:mb-0 order-1 xl:order-none">
               <ul className="flex flex-col gap-10">
                 {info.map((item, index) => {
                   return (
@@ -131,7 +117,40 @@ const Contact = () => {
               </ul>
             </div>
 
-          </div>
+          {/* <div className="xl:w-[46%] order-1 xl:order-1 flex flex-col gap-[30px]"> */}
+
+            {/* globe */}
+            {/* <div className="rounded-xl justify-end">
+              <p className="text-xl text-white/80 mb-4 text-center">
+                I'm very flexible with time zone communications
+              </p>
+              <div className="relative rounded-xl w-full h-64 overflow-hidden justify-center ithems-center">
+                <div className="absolute w-full h-full top-1/2 transform -translate-y-1/2 scale-95">
+                  <GlobeDemo />
+                </div>
+              </div>
+            </div> */}
+
+            {/* info */}
+            {/* <div className="flex-1 flex items-center xl:justify-center mb-8 xl:mb-0 order-1 xl:order-none">
+              <ul className="flex flex-col gap-10">
+                {info.map((item, index) => {
+                  return (
+                    <li key={index} className="flex items-center gap-6">
+                      <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                        <div className="text-[28px]">{item.icon}</div>
+                      </div>
+                      <div className="flex-1">
+                        <p className="text-white/60">{item.title}</p>
+                        <h3 className="text-xl">{item.description}</h3>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div> */}
+
+          {/* </div> */}
 
         </div>
 
