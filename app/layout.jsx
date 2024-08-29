ad0d55d13aacae5fb65f1e076f91da6396bcfa13
Manolo Iniguez <iniguezms@hotmailcom>
2024-08-29 15:57:08 -0500

    Fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 // components
 import Header from "@/components/Header";
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
           {children}
         </PageTransition>
         <Analytics />
+        <SpeedInsights />
         </body>
       
     </html>
