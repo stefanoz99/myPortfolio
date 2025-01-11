554b0b730da4458ffffa8b6ceb113db494abca3b
stefanoz99 <159219031+stefanoz99@users.noreply.github.com>
2025-01-10 23:18:53 -0500

    Update page.jsx

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -4,21 +4,21 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { motion } from "framer-motion";
 import { ExpandableCardDemo } from "../../components/ExpandableCardDemo";
 
-// Thesis data
-const thesis = [
+// Categories and Data
+const categories = [
   {
-    title: "Supply Demand Analysis in Automobile Markets",
-    description: "Modeling U.S. Automotive Demand: A Multi-Stage Analysis of Price Sensitivity, and Brand Competition",
-    filterTag: "automobile",
-  },
-  {
-    title: "Master of Information Systems with a Specialization in Business Intelligence and Data Analytics Thesis",
-    description: `Development of an unsupervised learning model to detect patterns and anomalies related to money laundering crimes, based on the processing of economic and social data in Ecuadorian cantons and provinces for the period 2013–2023. (Original thesis written in Spanish)`,
+    title: "Analytical Insights",
+    description: "Showcasing advanced data analysis work.",
     filterTag: "bigdata",
   },
   {
-    title: "Bachelor's in Economics Thesis",
-    description: "Influence of Accession to ICSID on Foreign Direct Investment.",
+    title: "Visual Storytelling",
+    description: "Presenting work in data visualization using Power BI and Kepler.gl.",
+    filterTag: "visualization",
+  },
+  {
+    title: "Economic Research",
+    description: "In-depth research in economics including thesis works.",
     filterTag: "economics",
   },
 ];
@@ -42,35 +42,35 @@ const Work = () => {
       className="flex items-center justify-center min-h-[80vh] py-12 xl:py-5"
     >
       <div className="container mx-auto">
-        <Tabs defaultValue="automobile" className="flex flex-col xl:flex-col gap-[60px] justify-center">
+        <Tabs defaultValue="bigdata" className="flex flex-col xl:flex-col gap-[60px] justify-center">
           {/* Tabs */}
           <TabsList className="flex flex-col xl:flex-row w-full max-w-[380px] xl:max-w-[1200px] mx-auto gap-6">
-            {thesis.map((item) => (
+            {categories.map((category) => (
               <TabsTrigger
-                key={item.filterTag}
-                value={item.filterTag}
-                onClick={() => handleTabClick(item.filterTag)}
+                key={category.filterTag}
+                value={category.filterTag}
+                onClick={() => handleTabClick(category.filterTag)}
               >
-                {item.title.toUpperCase()}
+                {category.title.toUpperCase()}
               </TabsTrigger>
             ))}
           </TabsList>
 
           {/* Content */}
           <div className="min-h-[70vh] w-full mt-10">
-            {thesis.map((item) => (
+            {categories.map((category) => (
               <TabsContent
-                key={item.filterTag}
-                value={item.filterTag}
+                key={category.filterTag}
+                value={category.filterTag}
                 className="w-full text-center"
-                id={item.filterTag}
+                id={category.filterTag}
               >
                 <div className="flex flex-col gap-[30px]">
                   <div className="flex flex-row items-center justify-center gap-10">
-                    <h3 className="text-4xl font-bold">{item.title}</h3>
+                    <h3 className="text-4xl font-bold">{category.title}</h3>
                   </div>
-                  <p className="max-w-[600px] text-white/60 mx-auto">{item.description}</p>
-                  <ExpandableCardDemo filterTag={item.filterTag} />
+                  <p className="max-w-[600px] text-white/60 mx-auto">{category.description}</p>
+                  <ExpandableCardDemo filterTag={category.filterTag} />
                 </div>
               </TabsContent>
             ))}
